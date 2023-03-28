2d1f35872fd3b23ac6066449cd1776129a7d639c
Jake Nead <jakenead@gmail.com>
2023-03-28 11:53:39 -0700

    Generate header DOM elements

--- src/home.js
+++ src/home.js
@@ -1,4 +1,51 @@
-export { home }
+export { homePage }
 
-const home = () => console.log('home module is working')
 
+function homePage () {
+    const content = document.getElementById('\content')
+    const header = document.createElement('header')
+    header.classList.add('header')
+    content.append(header)
+
+    const navLinks = document.createElement('div')
+    navLinks.classList.add('navLinks')
+    header.append(navLinks)
+
+        const homeLink = document.createElement('div')
+        homeLink.classList.add('home')
+        homeLink.textContent = 'Home'
+
+        const menuLink = document.createElement('div')
+        menuLink.classList.add('menu')
+        menuLink.textContent = 'Menu'
+
+        const contactLink = document.createElement('div')
+        contactLink.classList.add('contact')
+        contactLink.textContent = 'Contact Us'
+
+        navLinks.append(homeLink, menuLink, contactLink)
+
+    const h1 = document.createElement('h1')
+    h1. textContent = 'Restaurant Name'
+    header.append(h1)
+
+    const headerInfo = document.createElement('div')
+    headerInfo.classList.add('headerInfo')
+    header.append(headerInfo)
+
+    const phone = document.createElement('p')
+    phone.classList.add('headerPhone')
+    phone.textContent = '555-123-4567'
+    const address = document.createElement('p')
+    address.classList.add('headerAddress')
+    address.textContent = "987 65th St. Sitka, AK"
+    headerInfo.append(phone, address)
+}
+
+
+// const newElement = (el, att, cont) => {
+//     const element = document.createElement(el)
+//     element.setAttribute('class', att)
+//     element.textContent = cont
+//     return element
+// }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
-import {home} from "./home.js";
+import {homePage} from "./home.js";
 
-console.log('hello from index.js!')
+homePage()
 
-home()
+
+
+
+// document.querySelector('.homeLink').addEventListener('click', () => {
+//     clearPage()
+//     homePage()
+// })
+
+// document.querySelector('.menuLink').addEventListener('click', () => {
+//     clearPage()
+//     menuPage()
+// })
+
+// document.querySelector('.contactLink').addEventListener('click', () => {
+//     clearPage()
+//     contactPage()
+// })
+
+
+// function clearPage(){
+//     const content = document.querySelector('#content')
+//     while (content.firstChild) {
+//         document.removeChild(content.firstChild)
+//     }
+// }
+
+
+
+
+// paragliding site with home/adventures/contact us
