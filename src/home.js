d8c5e867ed119a0c6cd30b7fc15da10ef1e90a84
Jake Nead <jakenead@gmail.com>
2023-04-03 12:53:14 -0700

    Add a main logo font

--- src/home.js
+++ src/home.js
@@ -7,11 +7,11 @@ const pageContent = [
 {tag: 'div', attr: 'homeLink', content: 'Home', parent: 'navLinks'},
 {tag: 'div', attr: 'adventuresLink', content: 'Adventures', parent: 'navLinks'},
 {tag: 'div', attr: 'contactLink', content: 'Contact Us', parent: 'navLinks'},
-{tag: 'h1', attr: 'h1', content: 'Paraglide AK', parent: 'header'},
 {tag: 'div', attr: 'headerInfo', content: '', parent: 'header' },
 {tag: 'p', attr: 'headerPhone', content: '555-123-4567', parent:'headerInfo' },
 {tag: 'p', attr: 'headerAddress', content: '123 45th St. Anchorage, AK', parent:'headerInfo' },
 {tag: 'main', attr: 'main', content: '', parent: 'content'},
+{tag: 'h1', attr: 'h1', content: 'Paraglide  Alaska', parent: 'main'},
 {tag: 'h2', attr: 'cta', content: 'Come see Fairbanks from a whole new perspective!', parent: 'main'},
 {tag: 'button', attr: 'contactButton', content: 'Book a flight!', parent: 'main'},
 
@@ -32,11 +32,3 @@ function elementBuilder({tag, attr, content, parent}) {
 
   pageContent.forEach(obj => elementBuilder(obj))
 }
-
-
-// const newElement = (el, att, cont) => {
-//     const element = document.createElement(el)
-//     element.setAttribute('class', att)
-//     element.textContent = cont
-//     return element
-// }
