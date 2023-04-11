72167f7dc8b9e3ca0a0d443bc177941a58f1d1b7
Jake Nead <jakenead@gmail.com>
2023-04-10 18:48:43 -0700

    Style articles and update image attributes

--- src/contact.js
+++ src/contact.js
@@ -10,5 +10,21 @@ const contactContent = [
     {tag: 'p', attr: 'headerPhone', content: '555-123-4567', parent:'headerInfo' },
     {tag: 'p', attr: 'headerAddress', content: '123 45th St. Anchorage, AK', parent:'headerInfo' },
     {tag: 'main', attr: 'main', content: '', parent: 'content'},
+    {tag: 'div', attr: 'formContainer', content: '', parent: 'main'},
+    {tag: 'form', attr: '', content: '', parent:  'formContainer'},
+    // {tag: 'label', attr: {for:'full-name'} , content: 'Full Name', parent: 'formContainer'},
+    // {tag: 'input', attr: {type: 'text', id:'full-name', name:'full-name', required: ''}, content: '', parent: 'formContainer'},
 
+    // {tag: , attr: , content: , parent: },
 ]
+
+
+// contactus 
+// leave us a message and we'll get back to you as soon as we can. 
+
+// full name 
+// email address
+//message
+// submit 
+
+// map of the area
